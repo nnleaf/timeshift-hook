83d0e3ceb1efa7ac131b38544388bf2c9c0844e0
Nam <nn.hoang.nguyen@gmail.com>
2020-05-02 15:40:11 -0400

    Forgot brackets

--- timeshift-hook.sh
+++ timeshift-hook.sh
@@ -4,8 +4,8 @@ user="$1"
 
 #Configuration
 readonly CONF_FILE=/etc/pacman.d/timeshift-hook/timeshift-hook.conf
-readonly COMMENT=$(get_property "comment" "string"
-readonly TIME=$(get_propety "time" "string"
+readonly COMMENT=$(get_property "comment" "string")
+readonly TIME=$(get_propety "time" "string")
 
 find /run/timeshift/backup/timeshift/snapshots-ondemand -mmin -"$time" | grep $(date +%Y-%m-%d)
 if [ $? -eq 0 ]; then
