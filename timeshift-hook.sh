ab15b69d66fc241dd530bfb7ab4996b31739d641
Nam <nn.hoang.nguyen@gmail.com>
2020-05-02 16:07:05 -0400

    Fully working - Added configuration and get_property snippet

--- timeshift-hook.sh
+++ timeshift-hook.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 #Timeshift Hook allowing a pre-update snapshot
 user="$1"
-
 get_property() {
-    if [ ! -f $CONF_FILE ]; then
-        echo "$CONF_FILE not found! Using $1=$3" >&2;
+  if [ ! -f $CONF_FILE ]; then
+  echo "$CONF_FILE not found! Using $1=$3" >&2;
+    param_value=$3
+  else
+    param_value=`sed '/^\#/d' $CONF_FILE | grep $1 | tail -n 1 |\
+    cut -d "=" -f2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'`
+
+    if ([ "$2" == "boolean" ] && [ "$param_value" != true ] && [ "$param_value" != false ]) || \
+      ([ "$2" == "integer" ] && [[ ! "$param_value" =~ ^[-+]?([1-9][[:digit:]]*|1)$ ]]) || \
+      ([ "$2" == "string" ] && [ "$param_value" == "" ]) ; then
+        echo "Wrong paramater in $CONF_FILE. Using $1=$3" >&2
         param_value=$3
-    else
-        param_value=`sed '/^\#/d' $CONF_FILE | grep $1 | tail -n 1 |\
-        cut -d "=" -f2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'`
-
-        if ([ "$2" == "boolean" ] && [ "$param_value" != true ] && [ "$param_value" != false ]) || \
-           ([ "$2" == "integer" ] && [[ ! "$param_value" =~ ^[-+]?([1-9][[:digit:]]*|1)$ ]]) || \
-           ([ "$2" == "string" ] && [ "$param_value" == "" ]) ; then
-            echo "Wrong paramater in $CONF_FILE. Using $1=$3" >&2
-            param_value=$3
-        fi
     fi
+  fi
 
-    echo $param_value
+  echo $param_value
 }
 
 #Configuration
 readonly CONF_FILE=/etc/pacman.d/timeshift-hook/timeshift-hook.conf
+readonly LOCATION=$(get_property "location" "string")
 readonly COMMENT=$(get_property "comment" "string")
 readonly TIME=$(get_property "time" "integer")
 
-find /run/timeshift/backup/timeshift/snapshots-ondemand -mmin -"$TIME" | grep $(date +%Y-%m-%d)
+find "$LOCATION" -mmin -"$TIME" | grep $(date +%Y-%m-%d)
 if [ $? -eq 0 ]; then
-    echo Last timeshift backup was less than "$TIME" minutes ago, aborting backup
+  echo Last timeshift backup was less than "$TIME" minutes ago, aborting backup
 else
-    /usr/bin/timeshift --create --comments "$COMMENT"
+  /usr/bin/timeshift --create --comments "$COMMENT"
 fi
